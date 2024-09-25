11c7400eeed9518653469ebbf350e3b25fe1d3cc
Allen Helton <allenheltondev@gmail.com>
2024-09-25 16:50:53 -0500

    final updates for functional game

--- functions/play-game.mjs
+++ functions/play-game.mjs
@@ -3,11 +3,22 @@ import { getAuthToken } from "./utils/momento.mjs";
 
 export const handler = async (event) => {
   try {
-    const user = await loadUser(event.requestContext.http.sourceIp);
+    const user = await loadUser(event.requestContext.identity.sourceIp);
     const momentoToken = await getAuthToken([{
-      role: 'readonly',
+      role: 'readwrite',
       cache: process.env.CACHE_NAME
-    }]);
+    },
+    {
+      role: 'subscribeonly',
+      cache: process.env.CACHE_NAME,
+      topic: 'location'
+    },
+    {
+      role: 'subscribeonly',
+      cache: process.env.CACHE_NAME,
+      topic: user.id
+    }
+  ]);
 
     const html = getGameHtml(user, momentoToken);
     return {
@@ -57,7 +68,7 @@ const getGameHtml = (user, token) => {
           <div id="game-window" class="w-3/4 bg-white rounded-lg shadow-lg p-4 mr-4 relative">
               <h2 class="text-2xl font-bold mb-4">Catch the squirrel</h2>
               <div id="game-area" class="bg-gray-200 h-50 w-fill rounded-lg relative">
-                  <img id="squirrel" src="https://i0.wp.com/www.gomomento.com/wp-content/uploads/2024/07/mo-small-1.png" alt="Squirrel" class="w-8 h-8 absolute" style="transition: all 0.3s ease-in-out;">
+                  <img id="squirrel" src="https://i0.wp.com/www.gomomento.com/wp-content/uploads/2024/07/mo-small-1.png" alt="Squirrel" class="w-8 h-8 absolute" style="transition: all 0.3s ease-in-out;" onclick="registerPoints()">
               </div>
           </div>
           <div id="player-list" class="w-1/4 bg-white rounded-lg shadow-lg p-4">
@@ -70,49 +81,110 @@ const getGameHtml = (user, token) => {
 
       <script>
           const token = "${token}";
-          const players = [
-              "Player 1",
-              "Player 2",
-              "Player 3",
-              "Player 4",
-          ];
+          let interval;
 
-          function populatePlayerList() {
-              const playerList = document.getElementById("players");
-              players.forEach(player => {
-                  const li = document.createElement("li");
-                  li.textContent = player;
-                  li.className = "bg-gray-100 p-2 rounded";
-                  playerList.appendChild(li);
-              });
+          function moveSquirrel(x, y, id) {
+            const squirrel = document.getElementById('squirrel');
+            const gameArea = document.getElementById('game-area');
+            const cellSizeX = gameArea.clientWidth / 10;
+            const cellSizeY = gameArea.clientHeight / 10;
+
+            const newX = (x - 1) * cellSizeX;
+            const newY = (y - 1) * cellSizeY;
+
+            squirrel.style.left = newX + 'px';
+            squirrel.style.top = newY+ 'px';
+            squirrel.style.cursor = 'pointer';
+            squirrel.name = id;
           }
 
-          function moveSquirrel(x, y) {
-              const squirrel = document.getElementById('squirrel');
-              const gameArea = document.getElementById('game-area');
-              const cellSize = gameArea.clientWidth / 10; // Divide game area into 10x10 grid
-
-              // Calculate position (subtracting 1 to make it 0-indexed)
-              const newX = (x - 1) * cellSize;
-              const newY = (y - 1) * cellSize;
-
-              squirrel.style.left = newX + 'px';
-              squirrel.style.top = newY+ 'px';
+          function updateCoordinates() {
+            const x = Math.floor(Math.random() * 10) + 1;
+            const y = Math.floor(Math.random() * 10) + 1;
+            const id = generateUUID();
+            fetch('https://api.cache.cell-us-east-1-1.prod.a.momentohq.com/cache/${process.env.CACHE_NAME}/location?ttl_seconds=300', {
+              method: 'PUT',
+              headers: {
+                'Authorization': '${token}',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ x, y, id })
+            });
+            moveSquirrel(x, y, id);
           }
 
-          // Simulating coordinate updates (replace this with your actual update mechanism)
-          function simulateCoordinateUpdates() {
-              const x = Math.floor(Math.random() * 10) + 1;
-              const y = Math.floor(Math.random() * 10) + 1;
-              moveSquirrel(x, y);
+          function registerPoints() {
+            const squirrel = document.getElementById('squirrel');
+            const response = fetch('/v1/squirrels', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ id: squirrel.name })
+            });
+            if(response.ok){
+              updateCoordinates();
+              clearInterval(interval);
+              interval = setInterval(updateCoordinates, 2000);
+            }
+          }
+
+          function generateUUID() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+              const r = Math.random() * 16 | 0;
+              const v = c === 'x' ? r : (r & 0x3 | 0x8);
+              return v.toString(16);
+            });
           }
 
           window.onload = function() {
-              populatePlayerList();
-              moveSquirrel(1, 1); // Start at position (1,1)
-              // Simulate coordinate updates every 3 seconds
-              setInterval(simulateCoordinateUpdates, 3000);
+              moveSquirrel(1, 1, generateUUID());
+              // Simulate coordinate updates every 2 seconds
+              interval = setInterval(updateCoordinates, 2000);
+              pollForLocation();
+              pollForUserUpdates();
           };
+
+          async function pollForLocation() {
+            try {
+              const response = await fetch('https://api.cache.cell-us-east-1-1.prod.a.momentohq.com/topics/${process.env.CACHE_NAME}/location', {
+                  headers: {
+                    'Authorization': '${token}'
+                  }
+                });
+              if(response.ok){
+                const data = await response.json();
+                const location = JSON.parse(data.items[0].item.value.text);
+                moveSquirrel(location.x, location.y, location.id);
+              }
+            } catch (err) {
+              console.error(err);
+            } finally {
+              setTimeout(pollForLocation, 0);
+            }
+          }
+
+          async function pollForUserUpdates() {
+            try {
+              const response = await fetch('https://api.cache.cell-us-east-1-1.prod.a.momentohq.com/topics/${process.env.CACHE_NAME}/${user.id}', {
+                  headers: {
+                    'Authorization': '${token}'
+                  }
+                });
+              if(response.ok){
+                const data = await response.json();
+                const player = JSON.parse(data.items[0].item.value.text);
+                console.log(player)
+                const playerContainer = document.getElementById('${user.id}');
+                playerContainer.innerHTML = '${user.username} (Level ' + player.level + ')';
+              }
+            } catch (err) {
+              console.error(err);
+            } finally {
+              setTimeout(pollForUserUpdates, 0);
+            }
+          }
+
       </script>
   </body>
 </html>`;
--- functions/utils/momento.mjs
+++ functions/utils/momento.mjs
@@ -1,13 +1,11 @@
 import { AuthClient, CredentialProvider, ExpiresIn, GenerateDisposableToken } from '@gomomento/sdk';
-import { getSecret } from '@aws-lambda-powertools/parameters/secrets';
 
 let authClient;
 
 export const getAuthToken = async (permissions) => {
   if (!authClient) {
-    const secrets = await getSecret(process.env.SECRET_ID, { transform: 'json' });
     authClient = new AuthClient({
-      credentialProvider: CredentialProvider.fromString(secrets.momento)
+      credentialProvider: CredentialProvider.fromEnvironmentVariable('MOMENTO_API_KEY')
     });
   }
 
